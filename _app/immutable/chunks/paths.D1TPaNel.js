47acf7da725fb01a07514b38a018126dba0169dd
Xzoky174 <xzoky123@gmail.com>
2024-12-01 17:34:23 +0400

    Updated Again

---
+++ _app/immutable/chunks/paths.D1TPaNel.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1edfmhn)==null?void 0:s.base)??"/iasmun";var e;const t=((e=globalThis.__sveltekit_1edfmhn)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.CDjXVg4V.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CGS1rHuc.js";export{t as start};
